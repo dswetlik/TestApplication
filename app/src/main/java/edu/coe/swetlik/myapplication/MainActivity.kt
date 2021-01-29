a918a3be037699f5ff98016afee679746a23a6f3
Diedrich Swetlik <diedrich.swetlik@hotmail.com>
2021-01-29 14:43:15 -0600

    Working Counter and Total Price

--- app/src/main/java/edu/coe/swetlik/myapplication/MainActivity.kt
+++ app/src/main/java/edu/coe/swetlik/myapplication/MainActivity.kt
@@ -6,8 +6,13 @@ import android.widget.Toast
 import android.util.Log
 import android.view.View
 import android.widget.TextView
+import java.util.function.ToIntFunction
 
 class MainActivity : AppCompatActivity() {
+
+    var hotDogPrice:Int = 2
+    var hotDogCount:Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,12 +21,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun greet(v:View) {
-        Toast.makeText(this, "Toasty", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "Toasty", Toast.LENGTH_SHORT).show();
         changeTxt(v);
     }
 
     fun changeTxt(v:View) {
         var g = this.findViewById<TextView>(R.id.output_price_view);
-        g.text = "Button Press Boyyyyy";
+        g.setText("12");
+        var num = g.text.toString().toInt();
+    }
+
+    fun increaseAmount(v: View)
+    {
+        var g = this.findViewById<TextView>(R.id.output_price_view)
+        var i = this.findViewById<TextView>(R.id.count_text)
+        hotDogCount++
+        g.setText((hotDogCount * hotDogPrice).toString());
+        i.setText(hotDogCount.toString());
+    }
+
+    fun decreaseAmount(v: View)
+    {
+        var g = this.findViewById<TextView>(R.id.output_price_view)
+        var i = this.findViewById<TextView>(R.id.count_text)
+
+        hotDogCount--
+        if(hotDogCount < 0)
+            hotDogCount = 0
+        g.setText((hotDogPrice * hotDogCount).toString());
+        i.setText(hotDogCount.toString());
     }
 }
