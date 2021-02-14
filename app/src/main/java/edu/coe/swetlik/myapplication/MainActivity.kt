e39082ac8210f4342defe4d4ec629e0e82d2517f
Diedrich Swetlik <diedrich.swetlik@hotmail.com>
2021-02-14 14:48:38 -0600

    Cleanup of unused code

--- app/src/main/java/edu/coe/swetlik/myapplication/MainActivity.kt
+++ app/src/main/java/edu/coe/swetlik/myapplication/MainActivity.kt
@@ -33,13 +33,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /*
-        setOnClick(R.id.incHotdog, R.id.countHotdog, R.id.priceHotdog,true)
-        setOnClick(R.id.decHotdog, R.id.countHotdog, R.id.priceHotdog,false)
-        setOnClick(R.id.incSoda, R.id.countSoda, R.id.priceSoda,true)
-        setOnClick(R.id.decSoda, R.id.countSoda, R.id.priceSoda, false)
-        */
-
         createNewItem("Hot Dog", 1)
         createNewItem("Hamburger", 2)
         createNewItem("Soda", 1)
@@ -62,53 +55,6 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun greet(v:View) {
-
-        for(i in 0 until this.findViewById<LinearLayout>(R.id.ItemContainer).childCount step 1)
-        {
-            var item: Pair<View, UpDownBox> = this.findViewById<LinearLayout>(R.id.ItemContainer).getChildAt(i) to UpDownBox(this)
-            itemList.add(item.second)
-            //item.second.name = "Hamburger"
-
-            Log.i("Variable Test", "Added ${item.second.name}")
-        }
-
-        itemList[0].name = "Hot Dog"
-        itemList[0].price = 2
-        itemList[1].name = "Soda"
-        itemList[1].price = 1
-
-        Log.i("Variable Test", "Name of itemList object: ${itemList[0].name}")
-    }
-
-    fun increaseAmount(v: View?, countId:Int, priceId:Int)
-    {
-        val g = this.findViewById<TextView>(R.id.output_price_view)
-        val h = this.findViewById<TextView>(countId)
-        val i:Int = Integer.valueOf(this.findViewById<TextView>(priceId).text.toString())
-        var j:Int = Integer.valueOf(this.findViewById<TextView>(countId).text.toString())
-
-        j++
-
-        h.setText(j.toString())
-        updatePrice()
-    }
-
-    fun decreaseAmount(v: View?, countId:Int, priceId: Int)
-    {
-        val g = this.findViewById<TextView>(R.id.output_price_view)
-        val h = this.findViewById<TextView>(countId)
-        val i:Int = Integer.valueOf(this.findViewById<TextView>(priceId).text.toString())
-        var j:Int = Integer.valueOf(this.findViewById<TextView>(countId).text.toString())
-
-        j--
-        if(j < 0)
-            j = 0
-
-        h.setText(j.toString())
-        updatePrice()
-    }
-
     fun updatePrice()
     {
         var totalPrice:Int = 0
@@ -127,16 +73,4 @@ class MainActivity : AppCompatActivity() {
         updatePrice()
     }
 
-    fun setOnClick(buttonId:Int, textId:Int, priceId: Int, isIncrease:Boolean)
-    {
-        val btn:Button = this.findViewById<Button>(buttonId)
-        btn.setOnClickListener(object: View.OnClickListener {
-            override fun onClick(v: View?) {
-                if (isIncrease)
-                    increaseAmount(v, textId, priceId)
-                else
-                    decreaseAmount(v, textId, priceId)
-            }
-        })
-    }
 }
